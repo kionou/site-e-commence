a670e5d7930e3ba36d1946461e68a6af09da5a18
kionou <kionoumamadou.00@gmail.com>
2022-05-12 18:06:49 +0000

    18h06mn

--- controllers/AccueilControl.js
+++ controllers/AccueilControl.js
@@ -3,7 +3,7 @@ const { request ,response } = require("express");
 
 const controlleur = class{
     static Accueil = (req=request,res=response)=>{
-        res.render('nav')
+        res.render('index')
 
     }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,38 @@
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
+// const navToggle = document.querySelector(".navbar_toggle");
+// const links = document.querySelector(".main_nav");
 
-navToggle.addEventListener('click', function(){
-    links.classList.toggle("show_nav");
-})
+// navToggle.addEventListener('click', function(){
+//     links.classList.toggle("show_nav");
+// })
+
+
+
+$('.content-slick').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    // autoplay: true,
+    cssEase: 'linear'
+  });
+
+
+
+$('.container-cadre').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
+
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});
